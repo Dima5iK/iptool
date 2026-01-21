6bde7db39fd4f6e9447a21cad82f7a63ded3382d
Михайлов Дмитрий Эдуардович <de-mikhailov@ekra.local>
2026-01-21 10:49:19 +0300

    Отказ от класса IP

--- logic.py
+++ logic.py
@@ -7,25 +7,32 @@ import subprocess, threading,json
 
 
 class IP:
+    """Слишком сложно, не используется"""
     def __init__(self, ip:str, cidr:str):
         self.ip:str = ip
         self.cidr:str = str(cidr)
-
-
+    
+    def get_ip(self) -> str:
+        return self.ip
+    
+    def get_cidr(self) -> str:
+        return self.cidr
+    
 class NIC:
-    def __init__(self, index:int, name:str, description:str, ip_addresses:list[IP], 
+    def __init__(self, index:int, name:str, description:str, ip_addresses:list, 
                  mac:str, status:bool, speed:int, received_bytes:int, sent_bytes:int):
         self.index:int = index
         self.name:str = name
         self.description = description
-        self.ip_addresses:list[IP] = ip_addresses  # Список строк
+        self.ip_addresses:list = ip_addresses  # Список строк
         self.mac:str = mac
         self.status:bool = status  # "Up"/"Down"
         self.speed:int = int(speed)
         self.received_bytes:int = int(received_bytes)
         self.sent_bytes:int = int(sent_bytes)
 
-
+    def get_ip_cidr(self):
+        return self.ip_addresses.get_ip() + '/' + self.ip_addresses.get_cidr()
 
 
 class Route:
@@ -52,6 +59,51 @@ class PowerShellMonitor:
         self.new_data_flag:bool = False         
         """Флаг новых данных"""
 
+
+    def get_IP_obj_from_str(self,ip_str_list:list[str]) ->list[IP] :
+        """Из строки 'ip/mask' получаем ip и mask"""
+        ip_obj:list[IP] = []
+        
+        if isinstance(ip_str_list,str):
+            """если на интерфейсе адресов = 1"""
+            ip,cidr = ip_str_list.split('/')
+            ip_obj.append(IP(ip,cidr))
+
+        elif isinstance(ip_str_list,list):
+            """если на интерфейсе адресов > 1"""
+            for ip_str in ip_str_list:
+                
+                ip,cidr = ip_str.split('/')
+                ip_obj.append(IP(ip,cidr))
+        elif ip_str_list == {}:
+            """если нет адреса"""
+            ip_obj.append(IP(None,None))
+        return ip_obj
+
+
+    def _parse_json_to_nics(self, data):
+        """реобразует сырой JSON в список объектов NIC
+"""
+        nics:list[NIC] = []
+        
+        for adapter_data in data:
+            
+            nic = NIC(
+                index=adapter_data.get('Index'),
+                name=adapter_data.get('Name'),
+                description=adapter_data.get('Descrip'),
+                #ip_addresses=self.get_IP_obj_from_str(adapter_data.get('IPAddress', [])), слишком сложно
+                ip_addresses=adapter_data.get('IPAddress', []),
+                mac=adapter_data.get('MacAddress'),
+                status=adapter_data.get('Status'),
+                speed=str(adapter_data.get('Speed', '0')),
+                received_bytes=int(adapter_data.get('ReceivedBytes', 0)),
+                sent_bytes=int(adapter_data.get('SentBytes', 0))
+            )
+            nics.append(nic)
+        return nics
+
+
     def start(self):
         """Запускает PowerShell с заданным скриптом"""
         self.running = True
@@ -77,23 +129,8 @@ class PowerShellMonitor:
                     json_data = json.loads(line.strip())
                     nic_list:list[NIC] = []
                     #парсинг
-                    for adapter in json_data:
-                        
-
-                        
-                        nic = NIC(
-                            index=adapter.get('Index'),
-                            name=adapter.get('Name'),
-                            description=adapter.get('Descrip'),
-                            ip_addresses=ip_list,
-                            mac=adapter.get('MacAddress'),
-                            status=adapter.get('Status'),
-                            speed=str(adapter.get('Speed', '0')),
-                            received_bytes=int(adapter.get('ReceivedBytes', 0)),
-                            sent_bytes=int(adapter.get('SentBytes', 0))
-                        )
-                    
-                        nic_list.append(nic)
+                    nic_list = self._parse_json_to_nics(json_data)
+                                                               
                     with self.lock:
                         self.new_data = nic_list
                         self.new_data_flag = True
@@ -102,36 +139,9 @@ class PowerShellMonitor:
 
 
     def stop(self):
-        """ Не актуально Останавливает мониторинг"""
+        """Останавливает мониторинг"""
         self.running = False
         if self.proc:
             self.proc.terminate()
-    def get_IP_obj_from_str(ip_str_list:list[str]) ->list[IP] :
-        """Из строки 'ip/mask' получаем ip и mask"""
-        ip_obj:list[IP] = []
-        for ip_str in ip_str_list:
-            ip,cidr = ip_str.split('/')
-            ip_obj.append(IP(ip,cidr))
-        return ip_obj
 
-
-    def _parse_json_to_nics(self, json_data):
-        """реобразует сырой JSON в список объектов NIC
-"""
-        nics = []
-        if json_data:
-            for adapter_data in json_data:
-                nic = NIC(
-                    index=adapter_data.get('Index'),
-                    name=adapter_data.get('Name'),
-                    description=adapter_data.get('Descrip'),
-                    ip_addresses=adapter_data.get('IPAddress', []),
-                    mac=adapter_data.get('MacAddress'),
-                    status=adapter_data.get('Status'),
-                    speed=str(adapter_data.get('Speed', '0')),
-                    received_bytes=int(adapter_data.get('ReceivedBytes', 0)),
-                    sent_bytes=int(adapter_data.get('SentBytes', 0))
-                )
-                nics.append(nic)
-            return nics
 
--- testing.py
+++ testing.py
@@ -11,7 +11,7 @@ for iter in range(5):
     time.sleep(1.5)
     nics = monitor.new_data
     for nic in nics:
-        #print(nic.description,nic.name, nic.ip_addresses, nic.mac)
+        print(nic.description,nic.name, nic.ip_addresses, nic.mac)
     #print(monitor.new_data)
         pass
     print("________________________________")
